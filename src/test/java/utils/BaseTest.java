dad3ea444f9c2c43e5edac41d6b6cbd12b54d213
1bo999 <ihuseynli1604@gmail.com>
2025-12-09 00:30:41 +0400

    Added BaseTest.java

---
+++ src/test/java/utils/BaseTest.java
@@ -0,0 +1,22 @@
+package utils;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected RequestSpecification request;
+    private String token = ConfigReader.get("token");
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://api.themoviedb.org/3";
+
+        request = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Bearer " + token);
+    }
+}
